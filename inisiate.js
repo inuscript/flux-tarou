00e81ed82736c703e4c124467b23f083f89b32b1
inuscript <inuscript@gmail.com>
2016-05-31 09:49:19 +0900

    fib1

--- inisiate.js
+++ inisiate.js
@@ -2,15 +2,18 @@ class Component {
   constructor(){
     this.state = {
       value: 0
+      value2: 1
     }
   }
-  handleClick(value){
-    this.setState({value: value})
+  handleClick(value, value2){
+    this.setState({value: value, value2: value2})
   }
-  render({handleClick, value}){
+  render(){
+    let {value, value2} = this.state
+    let handleClick = this.handleClick.bind(this)
     return <div>
-      {value}
-      <button onClick={ e => handleClick(value + 1)}> + </button>
+      {value2}
+      <button onClick={ e => handleClick(value2, value + value2)}> fib </button>
     </div>
   }
 }
